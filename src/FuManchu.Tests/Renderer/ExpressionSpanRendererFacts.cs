69ca20cb4f0eb1482233ce3253634dc04e684914
Matthew Abbott <me@fidelitydesign.net>
2015-01-07 10:36:59 +0000

    Merged feature-escape-ampersand, for #6, #8

---
+++ src/FuManchu.Tests/Renderer/ExpressionSpanRendererFacts.cs
@@ -0,0 +1,34 @@
+﻿namespace FuManchu.Tests.Renderer
+{
+	using Xunit;
+
+	public class ExpressionSpanRendererFacts : ParserVisitorFactsBase
+	{
+		[Fact]
+		public void EncodesHtmlContent()
+		{
+			string template = "{{this}}";
+			string expected = "&lt;h1&gt;Hello World&lt;/h1&gt;";
+
+			RenderTest(template, expected, "<h1>Hello World</h1>");
+		}
+
+		[Fact]
+		public void RendersHtmlContentUsingTripleBraces()
+		{
+			string template = "{{{this}}}";
+			string expected = "<h1>Hello World</h1>";
+
+			RenderTest(template, expected, "<h1>Hello World</h1>");
+		}
+
+		[Fact]
+		public void RendersHtmlContentUsingAmpersand()
+		{
+			string template = "{{&this}}";
+			string expected = "<h1>Hello World</h1>";
+
+			RenderTest(template, expected, "<h1>Hello World</h1>");
+		}
+	}
+}
---
+++ src/FuManchu/SafeString.cs
@@ -0,0 +1,32 @@
+﻿namespace FuManchu
+{
+	using FuManchu.Text;
+
+	/// <summary>
+	/// Represents a safe string that doesn't support encoding, it is already considered to be encoded.
+	/// </summary>
+	public class SafeString : IEncodedString
+	{
+		private readonly object _value;
+
+		/// <summary>
+		/// Initialises a new instance of <see cref="SafeString"/>
+		/// </summary>
+		/// <param name="value">The value.</param>
+		public SafeString(object value)
+		{
+			_value = value;
+		}
+
+		/// <inheritdoc />
+		public string ToEncodedString()
+		{
+			if (_value == null)
+			{
+				return string.Empty;
+			}
+
+			return _value.ToString();
+		}
+	}
+}
